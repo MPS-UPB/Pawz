ff16af55b6e7920b9f2a28a3c869755313a4ce19
Mindroiu Andreea <andreea.mindroiu@gmail.com>
2013-01-09 21:29:53 +0200

    updated files

--- src/Utils.java
+++ src/Utils.java
@@ -1,16 +1,23 @@
 import java.io.File;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import com.sun.xml.xsom.XSParticle;
-
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.*;
 
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
@@ -42,114 +49,91 @@ public class Utils {
 		}
 	}
 		
-	public static void getXmlStringHash () throws ParserConfigurationException {
-		XSDParser parser = new XSDParser();
-		hash = new HashMap<String, ArrayList<String>>();
+	public static void createConfigFile () throws ParserConfigurationException {
+		if (xsdFiles == null) 
+			readFiles();
 		
-		for (int i = 0; i < xsdFiles.size(); i++) {
-			XSParticle[] particles = parser.parseXSD(new File(xsdFiles.get(i)));
-			XMLGenerator gen = new XMLGenerator();
-	        String[] execType = parser.getExecType().split(",");
+		XMLGenerator gen = new XMLGenerator();
+		String xmlString = gen.getXMLString(xsdFiles);
+		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();  
+        DocumentBuilder builder;  
 
-	        String s = gen.getXMLString(parser, xsdFiles.get(i), particles);
-	        for (int j = 0; j < execType.length; j++) {
-	            if (hash.containsKey(execType[j])) {
-	            	hash.get(execType[j]).add(s);
-	            }
-	            else {
-	            	ArrayList<String> arr = new ArrayList<String>();
-	            	arr.add(s);
-	            	hash.put(execType[j], arr);
-	            }
-	        }
-		}
-		
-		System.out.println(hash);
+        try  
+        {  
+            builder = factory.newDocumentBuilder();  
+  
+            Document document = builder.parse(new InputSource(new StringReader( xmlString ) ) );  
+  
+            TransformerFactory tranFactory = TransformerFactory.newInstance();  
+            Transformer trans = tranFactory.newTransformer();  
+            
+            trans.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            trans.setOutputProperty(OutputKeys.INDENT, "yes");
+            
+            Source src = new DOMSource(document);  
+            Result dest = new StreamResult(new File("configFile.xml"));  
+            trans.transform( src, dest );
+        } catch (Exception e)  
+        {  
+            e.printStackTrace();  
+        } 
 	}
 	
 	public static void parseXML(String fileName){
-		
+		List<Option> options = new ArrayList<Option>();
+
+		try {
+			DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+			Document doc = docBuilder.parse (new File(fileName));
+			
+			// normalize text representation
+			doc.getDocumentElement ().normalize ();
+
+			NodeList listOfOptions = doc.getElementsByTagName("option");
+
+			for (int i = 0; i < listOfOptions.getLength() ; i++){
+
+				String name=null;
+				HashMap<String,String> param = new HashMap<String, String>();
+				ArrayList<String> paramList = new ArrayList<String>();
+					
+				Node firstOptionNode = listOfOptions.item(i);
+				if(firstOptionNode.getNodeType() == Node.ELEMENT_NODE){
+					Element firstOptionElement = (Element)firstOptionNode;
+					NodeList nameList = firstOptionElement.getElementsByTagName("name");
+					Element nameElement = (Element)nameList.item(0);
 	
-	//	String fileName = "workflow.xml";
-		
-		  	
-    	List<Option> options = new ArrayList<Option>();
-    	
-    	
-    try {
-
-            DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-            Document doc = docBuilder.parse (new File(fileName));
-
-            // normalize text representation
-            doc.getDocumentElement ().normalize ();
-          
-            
-            NodeList listOfOptions = doc.getElementsByTagName("option");
-            int totalOptions = listOfOptions.getLength();
-            
-            for(int i=0; i<listOfOptions.getLength() ; i++){
-
-            	String name=null;
-            	HashMap<String,String> param = new HashMap<String, String>();
-            	ArrayList<String> paramList = new ArrayList<String>();
-            	
-                Node firstOptionNode = listOfOptions.item(i);
-                if(firstOptionNode.getNodeType() == Node.ELEMENT_NODE){
-
-
-                    Element firstOptionElement = (Element)firstOptionNode;
-
-                    NodeList nameList = firstOptionElement.getElementsByTagName("name");
-                    Element nameElement = (Element)nameList.item(0);
-
-                    NodeList textNameList = nameElement.getChildNodes();
-                    name = ((Node)textNameList.item(0)).getNodeValue().trim();
-                    
-                    NodeList listOfParam = firstOptionElement.getElementsByTagName("parameter");
-                    int totalParam = listOfParam.getLength();                 
-                    for (int k=0; k<totalParam; k++ ){
-                    	
-                    	  String key=null, value=null;
-                    	  Element paramElement = (Element)listOfParam.item(k);
-
-                          NodeList textParameterList = paramElement.getChildNodes();
-                          String aux[]=((Node)textParameterList.item(0)).getNodeValue().trim().split(",");
-                          key = aux[0].trim();
-                          value = aux[1].trim();
-                          param.put(key, value);
-                          paramList.add(key);
-                    	
-                    }
-                }//end if
-                Option op = new Option(name, param, paramList);
-                options.add(op);
-               
-
-            }//end for
-
-
-        }catch (SAXParseException err) {
-        System.out.println ("** Parsing error" + ", line " + err.getLineNumber () + ", uri " + err.getSystemId ());
-        System.out.println(" " + err.getMessage ());
-
-        }catch (SAXException e) {
-        Exception x = e.getException ();
-        ((x == null) ? e : x).printStackTrace ();
-
-        }catch (Throwable t) {
-        t.printStackTrace ();
-        }
-       
-    
-    
-    
-    
-    
-
-		
-		
+					NodeList textNameList = nameElement.getChildNodes();
+					name = ((Node)textNameList.item(0)).getNodeValue().trim();
+					
+					NodeList listOfParam = firstOptionElement.getElementsByTagName("parameter");
+					int totalParam = listOfParam.getLength();                 
+	
+					for (int k=0; k<totalParam; k++ ){
+						String key=null, value=null;
+						Element paramElement = (Element)listOfParam.item(k);
+						
+						NodeList textParameterList = paramElement.getChildNodes();
+						String aux[]=((Node)textParameterList.item(0)).getNodeValue().trim().split(",");
+						key = aux[0].trim();
+						value = aux[1].trim();
+						param.put(key, value);
+						paramList.add(key);
+					}
+				}//end if
+				Option op = new Option(name, param, paramList);
+				options.add(op);
+			}//end for
+		} catch (SAXParseException err) {
+			System.out.println ("** Parsing error" + ", line " + err.getLineNumber () + ", uri " + err.getSystemId ());
+			System.out.println(" " + err.getMessage ());
+		} catch (SAXException e) {
+			Exception x = e.getException ();
+			((x == null) ? e : x).printStackTrace ();
+		} catch (Throwable t) {
+			t.printStackTrace ();
+		}
 	}
-	
 }
--- src/XMLGenerator.java
+++ src/XMLGenerator.java
@@ -25,42 +25,81 @@ public class XMLGenerator {
     	doc = docBuilder.newDocument();
     }
         
-    public String getXMLString(XSDParser parser, String file, XSParticle[] particles) {
+    public String getXMLString(ArrayList<String> files) {
 		String xmlString = "";
+		HashMap <String, ArrayList<XSDParser>> parserHash = new HashMap<String, ArrayList<XSDParser>>();
+		HashMap <String, ArrayList<XSParticle[]>> particleHash = new HashMap<String, ArrayList<XSParticle[]>>();
+		ArrayList<String> execTypes = new ArrayList<String>();
 		
-        try {
+		for (int i = 0; i < files.size(); i++) {
+			XSDParser parser = new XSDParser();
+			XSParticle[] particles = parser.parseXSD(new File(files.get(i)));
+			
+			String[] execTypeVal = parser.getExecType().split(",");
+			for (int j = 0; j < execTypeVal.length; j++) {
+				if (parserHash.containsKey(execTypeVal[j]) && 
+						particleHash.containsKey(execTypeVal[j])) {
+					parserHash.get(execTypeVal[j]).add(parser);
+					particleHash.get(execTypeVal[j]).add(particles);
+				}
+				else {
+					execTypes.add(execTypeVal[j]);
+					
+					ArrayList<XSDParser> parserArr = new ArrayList<XSDParser>();
+					ArrayList<XSParticle[]> particleArr = new ArrayList<XSParticle[]>();
+					parserArr.add(parser);
+					particleArr.add(particles);
+					parserHash.put(execTypeVal[j], parserArr);
+					particleHash.put(execTypeVal[j], particleArr);
+				}
+			}
+			
+		}
+
+		try {
             //create the root element and add it to the document
             Element root = doc.createElement("config");
             doc.appendChild(root);
 
-            //create child element, add an attributes, and add to root
-            Element child = doc.createElement("execName");
-            String value = parser.getExecName();
-            child.setAttribute("name", value);
-            root.appendChild(child);
-            String propertyName = null;
-    		String propertyType = null;
-
-    		for (XSParticle p : particles) {
-    			XSTerm pTerm = p.getTerm();
-    			propertyName = pTerm.asElementDecl().getName();
-    			if (pTerm.asElementDecl().getType().getName() != null) {
-    				propertyType = pTerm.asElementDecl().getType().getName();
-    			}
-    			else 
-    				propertyType = pTerm.asElementDecl().getType().getBaseType().getName();
-    			
-    			if (propertyName.compareTo("execInfo") != 0 
-    				&& propertyName.compareTo("inputFile") != 0
-    				&& propertyName.compareTo("outputFile") != 0) {
-    			
-	    			Element innerChild = doc.createElement("parameter");
-	                innerChild.setAttribute("name", propertyName);
-	                innerChild.setAttribute("type", propertyType);
-	                child.appendChild(innerChild);
-    			}
-    		}
-    		
+            for (int i = 0; i < execTypes.size(); i++) {
+            	Element execTypeElem = doc.createElement("execType");
+            	execTypeElem.setAttribute ("name", execTypes.get(i));
+            	
+            	root.appendChild(execTypeElem);
+            	
+            	ArrayList<XSDParser> parsers = parserHash.get(execTypes.get(i));
+            	ArrayList<XSParticle[]> particles = particleHash.get(execTypes.get(i));
+            	
+            	for (int j = 0; j < parsers.size(); j++) {
+                    Element execNameElem = doc.createElement("execName");
+                    String value = parsers.get(j).getExecName();
+                    execNameElem.setAttribute("name", value);
+                    execTypeElem.appendChild(execNameElem);
+                    String propertyName = null;
+            		String propertyType = null;
+            		
+            		for (XSParticle p : particles.get(j)) {
+            			XSTerm pTerm = p.getTerm();
+            			propertyName = pTerm.asElementDecl().getName();
+            			if (pTerm.asElementDecl().getType().getName() != null) {
+            				propertyType = pTerm.asElementDecl().getType().getName();
+            			}
+            			else 
+            				propertyType = pTerm.asElementDecl().getType().getBaseType().getName();
+            			
+            			if (propertyName.compareTo("execInfo") != 0 
+            				&& propertyName.compareTo("inputFile") != 0
+            				&& propertyName.compareTo("outputFile") != 0) {
+            			
+        	    			Element innerChild = doc.createElement("parameter");
+        	                innerChild.setAttribute("name", propertyName);
+        	                innerChild.setAttribute("type", propertyType);
+        	                execNameElem.appendChild(innerChild);
+            			}
+            		}   		
+            	}
+            }
+            
             TransformerFactory transfac = TransformerFactory.newInstance();
             Transformer trans = transfac.newTransformer();
             trans.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
